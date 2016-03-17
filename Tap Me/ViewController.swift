dd4f55edee718df108163f66ace2208021b64c9e
Randall Mardus <randalljmardus@gmail.com>
2016-03-17 10:55:15 -0400

    setup timer countdown functionality

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -15,15 +15,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var timerLabel: UILabel!
     
     var count = 0
+    
+    var seconds = 0
+    var timer = NSTimer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        setupGame()
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupGame() {
+        seconds = 30
+        count = 0
+        
+        timerLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+    }
+    
+    func subtractTime() {
+        seconds--
+        timerLabel.text = "Time: \(seconds)"
+        
+        if(seconds == 0) {
+            timer.invalidate()
+        }
     }
     
     @IBAction func buttonPressed(sender: UIButton) {
